57261feb679683b17914bf69820fbf0c32e38ee1
anjana6 <anjanashakthi95@gmail.com>
2019-11-18 23:44:15 +0530

    create delete icon

--- contact-list/src/components/Contact.js
+++ contact-list/src/components/Contact.js
@@ -6,17 +6,23 @@ const Contact = (props) => {
 
     const onShowClick = (e)=>{
         setState({
-            showContactinfo:!state
+            showContactinfo:!state.showContactinfo
         }) 
 
     }
     return (
         <div className="card card-body mb-3">
-            <h4>{name} <i onClick={onShowClick} className="fa fa-sort-down"/></h4>
-            <ul className="list-group">
+            <h4>{name}
+             <i onClick={onShowClick} className="fa fa-sort-down" style={{cursor:'pointer'}}/>
+             <i className="fa fa-times" style={{cursor:'pointer',float: 'right', color: 'red'}}/>
+            </h4>
+            {state.showContactinfo? (
+                <ul className="list-group">
                 <li className="list-group-item">Email:{email}</li>
                 <li className="list-group-item">Phone:{phone}</li>
             </ul>
+            ) : null}
+            
         </div>
     );
 };
