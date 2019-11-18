eb57f76518cc8ffbd6912708bc0bbea10797c294
anjana6 <anjanashakthi95@gmail.com>
2019-11-18 23:23:54 +0530

    create state

--- contact-list/src/components/Contact.js
+++ contact-list/src/components/Contact.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 const Contact = (props) => {
-    const {name,email,phone} = props
+    const [state,setState] = useState({showContactinfo : true});
+    const {name,email,phone} = props;
+
+    const onShowClick = (e)=>{
+        setState({
+            showContactinfo:!state
+        }) 
+
+    }
     return (
         <div className="card card-body mb-3">
-            <h4>{name}</h4>
+            <h4>{name} <i onClick={onShowClick} className="fa fa-sort-down"/></h4>
             <ul className="list-group">
                 <li className="list-group-item">Email:{email}</li>
                 <li className="list-group-item">Phone:{phone}</li>
--- contact-list/src/components/Header.js
+++ contact-list/src/components/Header.js
@@ -3,12 +3,12 @@ import React from 'react';
 const Header = (props) =>{
     return(
         <div>
-            <nav class="navbar navbar-expand-sm navbar-dark bg-danger">
-                <div class="container">
+            <nav className="navbar navbar-expand-sm navbar-dark bg-danger">
+                <div className="container">
                     <a className="navbar-brand" href="/">{props.branding}</a>
-                        <ul class="navbar-nav">
-                            <li class="nav-item active">
-                                <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+                        <ul className="navbar-nav">
+                            <li className="nav-item active">
+                                <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
                             </li>
                         </ul>
                 </div>
