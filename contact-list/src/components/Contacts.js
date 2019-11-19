0376f8491c14c0217fc5dedb531d1dc64f28868c
anjana6 <anjanashakthi95@gmail.com>
2019-11-19 17:55:10 +0530

    update some code

--- contact-list/src/components/Contacts.js
+++ contact-list/src/components/Contacts.js
@@ -1,31 +1,54 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Contact from './Contact';
 
-class Contacts extends React.Component{
-    state = {contacts: [
-        {
-            id:1,
-            name: 'Anjana',
-            email: 'amjama@gmail.com',
-            phone: '222-333-2541'
-        },
-        {
-            id:2,
-            name: 'Anjana',
-            email: 'amjama@gmail.com',
-            phone: '222-333-2541'
-        }
-    ]}
-    render(){
-        const {contacts} = this.state;
-        return(
-            <div>
-                {contacts.map(contact => <Contact key={contact.id} name={contact.name} email={contact.email} phone={contact.phone}/>)}
-            </div>
+const Contacts = () =>{
+    const [state,setState] = useState({contacts : [
+                {
+                    id:1,
+                    name: 'Anjana',
+                    email: 'amjama@gmail.com',
+                    phone: '222-333-2541'
+                },
+                {
+                    id:2,
+                    name: 'Anjana',
+                    email: 'amjama@gmail.com',
+                    phone: '222-333-2541'
+                }
+            ]})
+    return(
+        
+        <div>
+            {state.contacts.map(contact => <Contact key={contact.id} name={contact.name} email={contact.email} phone={contact.phone}/>)}
+        </div>
+    );
+};
 
-        );
-    }
-}
+// class Contacts extends React.Component{
+//     state = {contacts: [
+//         {
+//             id:1,
+//             name: 'Anjana',
+//             email: 'amjama@gmail.com',
+//             phone: '222-333-2541'
+//         },
+//         {
+//             id:2,
+//             name: 'Anjana',
+//             email: 'amjama@gmail.com',
+//             phone: '222-333-2541'
+//         }
+//     ]}
+//     render(){
+//         const {contacts} = this.state;
+//         return(
+//             <div>
+//                 {contacts.map(contact => <Contact key={contact.id} name={contact.name} email={contact.email} phone={contact.phone}/>)}
+//             </div>
+
+//         );
+//     }
+// }
 
 // const Contacts = () =>{
 //     const [state,setState] = useState( contacts:[{}])
